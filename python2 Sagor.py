40185b2307cb7fa058a3ed24f697d8f893717461
Sk Sagor khan <91869176+sksagorahmed@users.noreply.github.com>
2021-10-04 16:14:33 +0600

    Update python2 Sagor.py

--- python2 Sagor.py
+++ python2 Sagor.py
@@ -1,45 +1,23 @@
 
-# BD Cloner TOOL
-# COMMANDS
-
-````
-$ pkg install python
-
-$ pkg install python2
-
+pkg update && pkg upgrade
+$ pkg install python3
 $ pkg install git
+$ pkg install pip && pkg install pip3
+$ pip3 install requests
+$ pip3 install colorama
+$ pip3 install bs4
+$ pip3 install html5lib
+$ pip3 install tqdm
+$ pip3 install cloudscraper
+$ git clone https://github.com/sagor/khan.git
+```
 
-$ pip2 install requests
-
-$ pip2 install mechanize
-
-$pip install requests
-
-$ pip install mechanize
-
-$ git clone https://github.com/Hunter-alamin/BDCLONER
-
-$ cd BDCLONER
-
-$ python2 BdCloner.py
-
-````
-
-# USERNAME & PASSWORDS
-````
-USERNAME : HUNTERBOY
-PASSWORD : alaminkhan
-````
-
-# Single Commands
-
-````
-$ pkg install python  ;  pkg install python2  ;  pkg install git  ;  pip2 install requests  ;  pip2 install mechanize  ;  pip install requests  ;  pip install mechanize  ;  git clone https://github.com/DevillHaunter/BDCLONER  ; 
- cd BDCLONER  ;  python2 BdCloner.py
-````
-# SCREENSHOT
-![](https://d.top4top.io/p_2018b8w5g0.jpg)
-
-### CONTACT WITH  HUNTERBOY ALAMIN IF YOU NEED ANY HELP
-<b>🔰🔰🔰🔰</b> </br></b></br> <br>[[![Github](https://img.shields.io/badge/Github-[HUNTERBOY_ALAMIN]-blue?style=flat-square&logo=GITHUBlogoColor=blue&labelColor=blue)](https://github.com/DevillHunter)] <br> [[![Github](https://img.shields.io/badge/TELEGRAM-[HUNTERBOY_ALAMIN]-red?style=flat-square&logo=TELEGRAMlogoColor=red&labelColor=cyan)](https://t.me/alamin123khan)]<br> [_[![Facebook](https://img.shields.io/badge/Facebook-HUNTERBOY_ALAMIN]-yellow?style=flat-square&logo=facebooklogoColor=green&labelColor=red)](https://www.facebook.com/alaminkhan.60)_]<br><b>🔰🔰🔰🔰
-
+## Tools Run :
+```
+$ ls 
+& cd sagor 
+& ls
+$ python2 sagor.py
+```
+Usernam. Sagor
+Password. Khan
